c5b35f48bb12830c0353fe83ae38644a9de4a6a8
Chung-Ying <ag.cyho@gmail.com>
2023-08-01 22:31:33 +0800

    add readme file

--- src/components/pages/Reserve.jsx
+++ src/components/pages/Reserve.jsx
@@ -1,7 +1,7 @@
 import Header from '../Header';
 import Footer from '../Footer';
 import Form from '../Form';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function Reserve(){
     // the state and function for storage and change the input value
@@ -23,6 +23,22 @@ export default function Reserve(){
 
     // change the available times
     const [availableTimes, setAvailableTimes] = useState(['17:00', '18:00'])
+    const updatedTimes = (prevTimes)=>{
+        setAvailableTimes([...prevTimes, '19:00'])
+    }
+
+    // set the api
+    useEffect(() => {
+    import('https://raw.githubusercontent.com/Meta-Front-End-Developer-PC/capstone/master/api.js')
+      .then((module) => {
+        // 在這裡可以使用 api.js 中的功能
+        module.fetchData();
+        // ...其他程式碼
+      })
+      .catch((error) => {
+        console.error('Failed to load api.js:', error);
+      });
+    }, [])
 
     return(
         <>
